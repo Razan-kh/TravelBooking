63bcdbecad3e302ddf86fb467d3c8949b421a23a
unknown <you@example.com>
2025-10-24 15:07:30 +0300

    feat: adding the Users folder to the Domain

---
+++ src/TravelBooking.Domain/Users/Repositories/IUserRepository.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Threading.Tasks;
+using TravelBooking.Domain.Users.Entities;
+using TravelBooking.Domain.Users.Entities;
+
+namespace TravelBooking.Domain.Users.Repositories;
+
+public interface IUserRepository
+{
+    Task<User?> GetByUsernameAsync(string username);
+    Task<User?> GetByEmailAsync(string email);
+    Task AddAsync(User user);
+    Task SaveChangesAsync();
+}
