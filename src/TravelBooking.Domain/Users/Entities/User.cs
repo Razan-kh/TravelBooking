747b301b663783f42e3798a5b1e763dfbbf29719
unknown <you@example.com>
2025-10-25 20:33:25 +0200

    Feat: Add hotels search functionality

--- src/TravelBooking.Domain/Users/Entities/User.cs
+++ src/TravelBooking.Domain/Users/Entities/User.cs
@@ -12,4 +12,5 @@ public sealed class User
     public string? Address { get; set; }
     public string PhoneNumber { get; set; }
     public UserRole Role { get; set; } = UserRole.User;
+    public virtual ICollection<Booking> Bookings { get; set; } = new List<Booking>();
 }
