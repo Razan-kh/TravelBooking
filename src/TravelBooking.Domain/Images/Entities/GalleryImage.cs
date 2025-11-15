00528289a2c140ae6062a589d7549a352e5ba2f3
unknown <you@example.com>
2025-11-15 09:19:27 +0200

    make the GalleryImage file to implement the BaseEntity

--- src/TravelBooking.Domain/Images/Entities/GalleryImage.cs
+++ src/TravelBooking.Domain/Images/Entities/GalleryImage.cs
@@ -1,7 +1,8 @@
+using TravelBooking.Domain.Shared.Entities;
+
 namespace TravelBooking.Domain.Images.Entities;
 
-public class GalleryImage
+public class GalleryImage : BaseEntity
 {
-    public Guid EntityId { get; set; }
     public string Path { get; set; } = string.Empty;
 }
