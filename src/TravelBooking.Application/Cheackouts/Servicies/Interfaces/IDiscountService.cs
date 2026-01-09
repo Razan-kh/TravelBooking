286fc7fdee3e208ecead03b1ea4f04d68a546d3a
unknown <you@example.com>
2026-01-09 20:14:58 +0200

    refactor(discounts): remove obsolete discount services

--- src/TravelBooking.Application/Cheackouts/Servicies/Interfaces/IDiscountService.cs
+++
@@ -1,8 +0,0 @@
-using TravelBooking.Domain.Carts.Entities;
-
-namespace TravelBooking.Application.Cheackout.Servicies.Interfaces;
-
-public interface IDiscountService
-{
-    decimal CalculateTotal(IEnumerable<CartItem> items);
-}
